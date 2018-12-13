d45bd5bd2f57749b15a4e7a655072efe8be4e338
Patryk Małek <patryk.malek@nauto.com>
2018-12-14 00:28:41 +0100

    Replacing imports to make build complete

--- SchemaHandler/SchemaHandlerJSON.go
+++ SchemaHandler/SchemaHandlerJSON.go
@@ -3,8 +3,8 @@ package SchemaHandler
 import (
 	"encoding/json"
 
-	"github.com/xitongsys/parquet-go/Common"
-	"github.com/xitongsys/parquet-go/parquet"
+	"github.com/nauto/parquet-go/Common"
+	"github.com/nauto/parquet-go/parquet"
 )
 
 type JSONSchemaItemType struct {
